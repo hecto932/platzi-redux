e06667c237715107906b097fcc7f5126f8e2cf5e
Hector Flores <hecto932@gmail.com>
2018-05-11 16:06:50 -0500

    Imprimiendo datos del estado

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -22,3 +22,13 @@ const store = createStore(
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+const $container = document.getElementById('playlist')
+const playlist = store.getState()
+playlist.forEach(e => {
+  const template = document.createElement('p')
+  template.textContent = e.title
+  $container.appendChild(template)
+})
+
+console.log(store.getState())
