420fedfca2e118df1c408b2749ad0f072a0aa764
Hector Flores <hecto932@gmail.com>
2018-05-15 09:19:12 -0500

    Agregando un Reducer para manejar los datos

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import reducer from '../reducers/data'
 import Home from '../pages/containers/home'
 import data from '../api.json'
 
---
+++ src/reducers/data.js
@@ -0,0 +1,11 @@
+function data (state, action) {
+  switch (action.type) {
+    case 'fasdasd': {
+      return state
+    }
+    default:
+      return state
+  }
+}
+
+export default data
